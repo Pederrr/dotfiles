1eb61bf4789af2d3fededf1de7436864ede57b07
Peter Romancik <promanci@redhat.com>
2025-06-18 11:43:26 +0200

    nvim: add AI 🗣️🗣️🗣️

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		'zbirenbaum/copilot.lua',
+		opts = {
+			suggestion = {
+				auto_trigger = true,
+				keymap = {
+					accept = '<M-L>',
+					accept_word = '<M-l>',
+				},
+			},
+			copilot_model = 'gpt-4o-copilot',
+			filetypes = {
+				yaml = true,
+				markdown = true,
+			},
+		},
+		-- enabled = false,
+	},
+}
