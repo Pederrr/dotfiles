e664da6dd21a20d20dede9cacd7e5e96a4f4fe2c
Peter Romancik <promanci@redhat.com>
2026-07-24 12:44:18 +0200

    nvim: update plugins, fix treesitter config for new version

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,57 +3,26 @@ return {
 		'nvim-treesitter/nvim-treesitter',
 		build = ':TSUpdate',
 		lazy = false,
-		opts = {
-			ensure_installed = {
-				'bash',
-				'c',
-				'html',
-				'lua',
-				'luadoc',
-				'markdown',
-				'markdown_inline',
-				'vim',
-				'vimdoc',
-				'query',
-			},
-			auto_install = true,
-			highlight = {
-				enable = true,
-				-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-				--  If you are experiencing weird indenting issues, add the language to
-				--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-				additional_vim_regex_highlighting = { 'ruby' },
-			},
-			indent = { enable = true, disable = { 'ruby' } },
-			textobjects = {
-				move = {
-					enable = true,
-					set_jumps = false,
-					goto_next_start = {
-						--- ... other keymaps
-						[']c'] = { query = '@code_cell.inner', desc = 'next code block' },
-					},
-					goto_previous_start = {
-						--- ... other keymaps
-						['[c'] = { query = '@code_cell.inner', desc = 'previous code block' },
-					},
-				},
-				select = {
-					enable = true,
-					lookahead = true,
-					keymaps = {
-						--- ... other keymaps
-						['ic'] = { query = '@code_cell.inner', desc = 'in block' },
-						['ac'] = { query = '@code_cell.outer', desc = 'around block' },
-					},
-				},
-			},
-		},
-		config = function(_, opts)
-			-- Prefer git instead of curl in order to improve connectivity in some environments
-			require('nvim-treesitter.install').prefer_git = true
-			---@diagnostic disable-next-line: missing-fields
-			require('nvim-treesitter.configs').setup(opts)
+		config = function()
+			local treesitter = require('nvim-treesitter')
+
+			local languages = {
+				'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'vim', 'vimdoc', 'query', 'python'
+			}
+			treesitter.install(languages)
+
+			-- allow highlighting for all filetypes that have a parser installed
+			vim.api.nvim_create_autocmd('FileType', {
+				pattern = languages,
+				callback = function() vim.treesitter.start() end,
+			})
+
+			-- folding
+			vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+			vim.wo[0][0].foldmethod = 'expr'
+
+			-- indentation
+			vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 		end,
 	},
 }
