d336c165e14f750b8ab18b76955292d405724c0d
Peter Romancik <promanci@redhat.com>
2026-07-24 12:54:49 +0200

    nvim: disable treesitter folding, cleanup config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,6 @@ return {
 		lazy = false,
 		config = function()
 			local treesitter = require('nvim-treesitter')
-
 			local languages = {
 				'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'vim', 'vimdoc', 'query', 'python'
 			}
@@ -14,15 +13,11 @@ return {
 			-- allow highlighting for all filetypes that have a parser installed
 			vim.api.nvim_create_autocmd('FileType', {
 				pattern = languages,
-				callback = function() vim.treesitter.start() end,
+				callback = function()
+					vim.treesitter.start()
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
 			})
-
-			-- folding
-			vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-			vim.wo[0][0].foldmethod = 'expr'
-
-			-- indentation
-			vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 		end,
 	},
 }
